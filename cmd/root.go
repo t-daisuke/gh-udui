76f92a6b44871c32b724c5237ec442393f70f378
t-daisuke <t.daisuke.64@gmail.com>
2025-04-06 18:52:10 +0900

    -hでちゃんと説明出てくれる。

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,13 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
+)
+
+var (
+	limit int
 )
 
 var rootCmd = &cobra.Command{
@@ -10,6 +16,14 @@ var rootCmd = &cobra.Command{
 	Long: `gh-udui is a GitHub CLI extension that helps you view PR comments, reviews, and discussions
 in a user-friendly format. It fetches PR information and displays the latest comments
 from non-bot users.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Fetching %d PRs...\n", limit)
+		// TODO: 実際のPR取得処理を実装
+	},
+}
+
+func init() {
+	rootCmd.PersistentFlags().IntVarP(&limit, "limit", "l", 5, "Number of PRs to fetch (default: 5)")
 }
 
 func Execute() error {
